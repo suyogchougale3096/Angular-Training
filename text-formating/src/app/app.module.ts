03eba7b06dbb46c8d3f60f1b1406f1065de4fb01
Suyog Sanjay Chougale <suyogch3300@gmail.com>
2024-07-04 16:37:42 +0530

    Created module and display component

--- text-formating/src/app/app.module.ts
+++ text-formating/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { TextDisplayComponent } from './components/home/text-display/text-display.component';
-import { FormatterComponent } from './components/home/formatter/formatter.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    TextDisplayComponent,
-    FormatterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- text-formating/src/app/modules/home/home.module.ts
+++ text-formating/src/app/modules/home/home.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { FormatterComponent } from './components/formatter/formatter.component';
+import { TextDisplayComponent } from './components/text-display/text-display.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    FormatterComponent,
+    TextDisplayComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
