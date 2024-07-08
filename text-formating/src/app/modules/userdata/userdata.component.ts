afa39e41ec57fa5635257c41f6ec2c24cf1ea76c
Suyog Sanjay Chougale <suyogch3300@gmail.com>
2024-07-08 22:51:12 +0530

    assignment 2 related changes

--- text-formating/src/app/modules/userdata/userdata.component.ts
+++ text-formating/src/app/modules/userdata/userdata.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators, Validator } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
-
 
 @Component({
   selector: 'app-userdata',
@@ -9,68 +8,62 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./userdata.component.css']
 })
 export class UserdataComponent implements OnInit {
+  database_name: string = "";
+  port_number: number = 0;
+  database_password: string = "";
+  database_url: string = "";
 
-  
-  // userForm: FormGroup;
-
-  // constructor(private formBuilder: FormBuilder) {
-  //   this.userForm = this.formBuilder.group({
-  //     fname: new FormControl(''),
-  //     lname: new FormControl('')
-  //   });
-  // }
-
-  database_name : string = "";
-  port_number : number = 0;
-  database_password : string = "";
-  database_url : string = "";
-
-
-  signup : FormGroup;
+  signup: FormGroup;
   storedForms: any[] = [];
 
-  constructor(private formBuilder : FormBuilder,private cookieService: CookieService){
+  constructor(private formBuilder: FormBuilder, private cookieService: CookieService) {
     this.signup = this.formBuilder.group({
-      dbname : new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z0-9]+$/)]),
-      portnumber : new FormControl('',[Validators.required,Validators.pattern(/^\d{1,5}$/)]),
-      dbpassword : new FormControl('',[Validators.required,Validators.minLength(7)]),
-      // dburl : new FormControl('',[Validators.required,this.urlValidator])
-      dburl : new FormControl('',[Validators.required,Validators.pattern(/^(https?:\/\/.*[^\/])$/)])
-      
-    })
+      dbname: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]),
+      portnumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{1,5}$/)]),
+      dbpassword: new FormControl('', [Validators.required, Validators.minLength(7)]),
+      dburl: new FormControl('', [Validators.required, Validators.pattern(/^(https?:\/\/.*[^\/])$/)])
+    });
   }
+
   ngOnInit(): void {
     this.signup.reset();
     this.getAllStoredForms();
   }
 
   onSubmit(): void {
-    // this.database_name = `${this.signup.get('dbname')?.value}`;
-    // this.port_number = Number(`${this.signup.get('portnumber')?.value}`);
-    // this.database_name = `${this.signup.get('dbpassword')?.value}`;
-
-    // const formValues = this.signup.value;
-    // const uniqueId = new Date().getTime();
-    // const formValuesString = JSON.stringify(formValues);
-    // this.cookieService.set(`signup_${uniqueId}`, formValuesString);
-    // console.log(`Form Data: ${formValuesString}`);
-    // this.signup.reset();
-    // this.getAllStoredForms();
-
-    // this.database_name = `${this.signup.get('dbname')?.value}`
     const formValues = this.signup.value;
+    let existingForms = [];
+
     const existingFormsString = this.cookieService.get('signup');
-    const existingForms = JSON.parse(existingFormsString);
+    if (existingFormsString) {
+      try {
+        existingForms = JSON.parse(existingFormsString);
+      } catch (e) {
+        console.error('Error parsing stored forms:', e);
+        // Optionally reset the cookie if parsing fails
+        this.cookieService.delete('signup');
+      }
+    }
+
     existingForms.push(formValues);
     this.cookieService.set('signup', JSON.stringify(existingForms));
     console.log(`Form Data: ${JSON.stringify(existingForms)}`);
-    
+
     this.signup.reset();
     this.getAllStoredForms();
   }
 
   getAllStoredForms(): void {
     const storedFormsString = this.cookieService.get('signup');
-    this.storedForms = JSON.parse(storedFormsString);
+    if (storedFormsString) {
+      try {
+        this.storedForms = JSON.parse(storedFormsString);
+      } catch (e) {
+        console.error('Error parsing stored forms:', e);
+        this.storedForms = [];
+      }
+    } else {
+      this.storedForms = [];
+    }
   }
 }
