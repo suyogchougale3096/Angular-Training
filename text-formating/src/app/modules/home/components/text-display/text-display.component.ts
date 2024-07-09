1364e05747d3aec1e3686f4ed1a4bbc6db6fd3f5
Suyog Sanjay Chougale <suyogch3300@gmail.com>
2024-07-09 14:59:07 +0530

    made changes in assignment1

--- text-formating/src/app/modules/home/components/text-display/text-display.component.ts
+++ text-formating/src/app/modules/home/components/text-display/text-display.component.ts
@@ -32,7 +32,7 @@ export class TextDisplayComponent implements OnInit, OnChanges {
       console.log("OnChange detect")
       console.log(changes['item'].currentValue)
       const btnValue = changes['item'].currentValue;
-      
+
       switch(btnValue){
         case 'clearallbtn':
           this.name = '';
@@ -43,43 +43,49 @@ export class TextDisplayComponent implements OnInit, OnChanges {
           this.result = this.name.replace(/\s+/g, '')
           break;
         case 'reverseallbtn':
-          
+
           this.result = this.name.split('').reverse().join('');
           break;
         case 'rspecialcharbtn':
-          
+
           this.result = this.name.replace(/[^a-zA-Z0-9 ]/g, '');
           break;
         case 'removestylebtn':
-          
+
           // this.name = "remove Style"
           const element = this.el.nativeElement.querySelector('.remove-style');
           this.renderer.removeAttribute(element, 'style');
           break;
         case 'capitalword':
-          
+
           this.result = this.name.toUpperCase();
           break;
         case 'boldbtn':
+          this.result = this.name;
             this.stylethis['font-weight'] = 'bold';
             break;
         case 'italicbtn':
+          this.result = this.name;
             this.stylethis['font-style'] = 'italic';
             break;
         case 'underlinebtn':
+          this.result = this.name;
             this.stylethis['text-decoration'] = 'underline';
-            
+
             break;
         case 'increasesizebtn':
+          this.result = this.name;
             this.fontSize += 10;
           this.stylethis['font-size'] = `${this.fontSize}px`;
             break;
 
         case 'decreasesizebtn':
+          this.result = this.name;
           this.fontSize -= 10;
             this.stylethis['font-size'] = `${this.fontSize}px`;
             break;
         default:
+          this.result = this.name;
           this.stylethis['color'] = `${this.item}`
           break;
       }
